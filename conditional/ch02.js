cb5364ca89b337c9b7451eee058e01a064919fa0
S <unknown33572@gmail.com>
2023-09-26 19:36:58 +0900

    switch

---
+++ conditional/ch02.js
@@ -0,0 +1,16 @@
+const answer = 'B';
+
+switch(answer) {
+  case 'A':
+    console.log(`스위치는 ${answer}입니다.`);
+    break;
+  case 'B':
+    console.log(`스위치는 ${answer}입니다.`);
+    break;
+  case 'C':
+    console.log(`스위치는 ${answer}입니다.`);
+    break;
+  default:
+    console.log('스위치는 A, B, C가 아닙니다. 다시 입력해주세요.');
+    break;
+}
