1628d7c7233fb99209dd836db876700e1a06159a
S <unknown33572@gmail.com>
2023-10-04 00:06:28 +0900

    rest-api

---
+++ rest_api/ch01.js
@@ -0,0 +1,13 @@
+//REST(Representational State Transfer)는 리소스를 이름으로 구분하여 해당 자원의 상태를 주고받는 모든 것을 의미함.
+//주로 JSON 형태로 데이터를 주고받는데, 최근에는 XML을 사용하는 경우도 있음.
+//REST API는 REST 기반으로 서비스 API를 구현한 것을 의미함.
+//REST는 기본적으로 웹의 기존 기술과 HTTP 프로토콜을 그대로 사용하기에 웹의 장점을 최대한 활용할 수 있음.
+//네트워크 상에서 클라이언트와 서버 사이의 데이터를 통신하기 위해 가장많이 사용되는 통신 방식 중 하나임.
+//REST API는 다음과 같은 구성 요소로 구성됨.
+//자원(Resource) - URI
+//행위(Verb) - HTTP METHOD
+//GET - Select(조회)
+//POST - Create(생성)
+//PUT - Update(수정)
+//DELETE - Delete(삭제)
+//REST API는 다음과 같은 특징을 가짐.
